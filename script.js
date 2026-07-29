001c8ff7d1cf4aebd770746a03c058d55675d710
Shubh_Maheta <shubhmaheta383@gmail.com>
2026-07-29 11:23:18 +0530

    add recommendation based on BMI

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-
 calbtn.addEventListener("click", () => {
-    let height = parseInt(document.getElementById("input1").value);
-    let weight = parseInt(document.getElementById("input2").value);
-    let ot = document.getElementById("cal");
-    let calbtn = document.getElementById("calbtn");
+  let height = parseInt(document.getElementById("input1").value);
+  let weight = parseInt(document.getElementById("input2").value);
+  let ot = document.getElementById("cal");
+  let calbtn = document.getElementById("calbtn");
   if (height === "" || height <= 0 || isNaN(height)) {
     ot.innerHTML = `Enter value is not valid ${height}`;
   } else if (weight === "" || weight <= 0 || isNaN(weight)) {
     ot.innerHTML = `Enter value is not number ${weight}`;
   } else {
     const BMI = (weight / ((height * height) / 10000)).toFixed(2);
-    ot.innerHTML=`<span>${BMI}</span>`
+    if (BMI <= 18.5) {
+      ot.innerHTML = `<span>You are Under Weight Based on ${BMI}</span>`;
+    } else if (BMI > 18.5 && BMI <=24.9) {
+      ot.innerHTML = `<span>You are Normal Weight Based on ${BMI}</span>`;
+    } else {
+      ot.innerHTML = `<span>You are Over Weight Based on ${BMI}</span>`;
+    }
   }
 });
